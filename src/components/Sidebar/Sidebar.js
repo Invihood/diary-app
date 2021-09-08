9fa2ffaef91629947107416c5f3ba99656f5e420
Valentin Shymanets <shymanets96@gmail.com>
2021-09-08 17:26:43 +0300

    added pattern background

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,7 +3,7 @@ const Sidebar = () => {
         <div className="sidebar"> 
             <aside className="left-menu">
                 <div className="folder-block">
-                    <div className="folder-title">Sidebar</div>
+                    <div className="folder-title"></div>
                 </div>
             </aside>
         </div> 
