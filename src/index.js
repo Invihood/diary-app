b47b0715b635b34d0507fa1803259af9582abf7f
Valentin Shymanets <shymanets96@gmail.com>
2021-09-14 18:58:57 +0300

    added Redux + reducers + Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { store } from './reducers';
 import './index.scss';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
